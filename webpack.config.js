c017f5aabe5d500ac3c5525866919117b9e9c312
Gangeya Upadhyaya <gangeya.u@gmail.com>
2025-12-12 19:52:05 +0530

    setting up

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: `${__dirname}/.webpack`,
+    filename: '[name].js'
+  }
+};
